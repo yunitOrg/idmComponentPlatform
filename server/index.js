4ab4e66d9a61ae9d8c4ac6306ac1437a9c034b87
chensq <chensq@dreamdt.cn>
2023-04-12 11:18:29 +0800

    fix:评论顶部不居中

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const history = require('connect-history-api-fallback')
 const path = require('path')
+const os = require('os')
 const fs = require('fs')
 const app = express()
 const port = 8088
 const distDir = path.join(__dirname, '../dist')
 
+const getIPAdress = function () {
+    const ips = []
+    const interfaces = os.networkInterfaces()
+    for (const devName in interfaces) {
+        const iface = interfaces[devName]
+        for (let i = 0; i < iface.length; i++) {
+            const alias = iface[i]
+            if (['IPv4', 'IPv6'].includes(alias.family) && alias.address !== '127.0.0.1' && !alias.internal && alias.address.indexOf('::') === -1) {
+                ips.push(alias.address)
+            }
+        }
+    }
+    return ips
+}
 app.use(history())
 
 if (fs.existsSync(distDir)) {
@@ -15,5 +30,10 @@ if (fs.existsSync(distDir)) {
 }
 
 app.listen(port, () => {
-    console.log(`server is running http://localhost:${port}`)
+    const ips = getIPAdress()
+    let ipsAddr = ''
+    ips.forEach((ip) => {
+        ipsAddr += `\r\n➜  Network: http://${ip}:${port}/`
+    })
+    console.log(`server is running \r\n➜  Local:   http://localhost:${port}/ ${ipsAddr}`)
 })
