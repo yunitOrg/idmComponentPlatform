e5e0265cfa2d913fe9da35b627cc8bacc4557cae
yunit <960504980@qq.com>
2023-04-12 14:52:47 +0800

    add：代理

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const history = require('connect-history-api-fallback')
 const path = require('path')
 const fs = require('fs')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const app = express()
 const port = 8088
 const distDir = path.join(__dirname, '../dist')
+
+const apiProxy = createProxyMiddleware({
+    target: 'http://localhost:8080',
+    changeOrigin: true,
+    ws: true
+})
+
+app.use(
+    '/idm-platform-java',
+    apiProxy
+)
 
 app.use(history())
 
